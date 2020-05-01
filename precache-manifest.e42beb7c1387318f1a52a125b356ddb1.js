ce5918fbd2e63588686011b653c21f4f2ecafec5
Celmad <manuelalaminosd@protonmail.com>
2020-05-01 16:43:46 +0200

    Updates

---
+++ precache-manifest.e42beb7c1387318f1a52a125b356ddb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983175a65ca83832aebc5847d0065477",
+    "url": "/robosocial/index.html"
+  },
+  {
+    "revision": "e982fa96548edf45b946",
+    "url": "/robosocial/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "1ae291c1036d07e01380",
+    "url": "/robosocial/static/css/main.23323e77.chunk.css"
+  },
+  {
+    "revision": "e982fa96548edf45b946",
+    "url": "/robosocial/static/js/2.9aa58bc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robosocial/static/js/2.9aa58bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae291c1036d07e01380",
+    "url": "/robosocial/static/js/main.298c238c.chunk.js"
+  },
+  {
+    "revision": "b9dbb84502a623fab36a",
+    "url": "/robosocial/static/js/runtime-main.4640ddde.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robosocial/static/media/SEGA.12a3db8b.woff"
+  }
+]);
